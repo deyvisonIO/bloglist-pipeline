53522413e811cb175922702a22df930c3adb4e03
Deyvison Iury <iurydeyvison@gmail.com>
2024-11-28 19:16:00 -0300

    Exercise 11.20 trying with playwright

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3001',
+    baseURL: 'http://localhost:6969',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -70,9 +70,10 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start:test',
-    cwd: '../backend',
-    url: 'http://127.0.0.1:3001',
+    // command: 'npm run start:test',
+    // cwd: '../backend',
+    // url: 'http://127.0.0.1:6969',
+    url: 'http://localhost:6969',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
