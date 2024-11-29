7688a6b098b1866bf30f438384bbe4fb926554db
Deyvison Iury <iurydeyvison@gmail.com>
2024-11-29 14:36:59 -0300

    Exercise 11.20 add health check periodic health check

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
-app.get('/health', (_, res) => {
+app.get('/api/health', (_, res) => {
   res.send('ok')
 })
 
